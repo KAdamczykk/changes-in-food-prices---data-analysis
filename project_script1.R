b47765fe7451280c6833598ec609517afdd9bca6
krzysztofadamczyk <01180690@pw.edu.pl>
2023-10-23 03:23:42 +0200

    template

---
+++ project_script1.R
@@ -0,0 +1,15 @@
+# TWD project 1
+# Opis
+# Loading libraries and data
+library(dplyr)
+library(ggplot2)
+library(readr)
+data <- read_csv("faostat_prices_in_europe.csv")
+# Tasks:
+
+
+
+
+
+# Plots:
+
