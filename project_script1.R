4116f21b510a190a857c395bf21deb95ebd413b4
Krzysztof Adamczyk <135175657+KAdamczykk@users.noreply.github.com>
2023-10-29 21:15:20 +0100

    changes

--- project_script1.R
+++ project_script1.R
@@ -5,7 +5,7 @@ library(dplyr)
 library(ggplot2)
 library(readr)
 library(lubridate)
-data <- read_csv("prc_fsc_idx__custom_8184163_linear.csv.gz")
+data <- read_csv("D:/Studia/Semestr 3/TWD/changes-in-food-prices---data-analysis/prc_fsc_idx__custom_8184163_linear.csv.gz")
 data  <- data.frame(data)
 # Tasks:
 #--------------------------------------------------------------------
@@ -78,6 +78,48 @@ thm <- thm %>%
   group_by(coicop) %>% 
   mutate(Inflacja = ((average - lag(average)) / lag(average)) * 100)
 thm <- na.omit(thm)
+#--------------------------------------------------------------------
+ map1 <- data %>%
+  mutate(Year = year(as.Date(paste0(TIME_PERIOD, "-01")))) %>% 
+  filter(Year == 2017) %>% 
+  group_by(coicop, geo) %>% 
+  summarise(cen2017 = mean(OBS_VALUE, na.rm = TRUE)) %>% 
+  ungroup()
+
+map2 <- data %>% 
+  mutate(Year = year(as.Date(paste0(TIME_PERIOD, "-01")))) %>% 
+  filter(Year == 2022) %>% 
+  group_by(coicop, geo) %>% 
+  summarise(cen2022 = mean(OBS_VALUE, na.rm = TRUE)) %>% 
+  ungroup()
+merged_map <- merge(x = map1, y = map2, on = (c(map1$coicop, map1$geo) == c(map2$coicop, map2$geo)))
+merged_map <- merged_map %>% 
+  filter(!(geo %in% c("EA19", "EA20", "EU27_2020"))) %>% 
+  mutate(Inflation = ((cen2022 -cen2017)/cen2017 )* 100) %>% 
+  group_by(geo) %>% 
+  slice(which.max(Inflation)) %>% 
+  select(coicop, geo, Inflation)
+
+dict <- data.frame(
+  SKR = c("AT", "BE", "BG", "CH", "CY", "CZ", "DE", "DK", "EE", "EL", "ES", "FI", "FR", "HR", "HU",
+          "IE", "IS", "IT", "LT", "LU" ,"LV", "MT" ,"NL", "NO" ,"PL", "PT", "RO", "SE", "SI", "SK",
+          "TR"),
+  Full = c("Austria", "Belgium", "Bulgaria", "Switzerland", "Czech Republic", "Cyprus", "Germany","Denmark", "Estonia",
+           "Greece", "Spain", "Finland", "France", "Croatia", "Hungary", "Ireland", "Iceland", "Italy",
+           "Lithuania", "Luxembourg", "Latvia", "Malta", "Netherlands", "Norway", "Poland", "Portugal", "Romania", "Sweden", "Slovenia", "Slovakia",
+           "Turkey")
+)
+merged_map <- merged_map %>%
+  left_join(dict, by=c("geo" = "SKR")) %>% 
+  select(coicop, Full, Inflation)
+food_dict <- data.frame(
+  SKR = c("CP01154", "CP01174", "CP01147", "CP0121",  "CP01124", "CP0115",  "CP0114"),
+  Full_food = c("Other edible oils", "Potatoes", "Eggs", "Coffe, tea, cocoa", "Poultry", "Oil, fats", "Milk, cheese")
+)
+merged_map <- merged_map %>% 
+  left_join(food_dict, by=c("coicop" = "SKR")) %>% 
+  select(Full_food, Inflation, Full)
+
 
 # Plots:
 ggplot(POLGEREU, aes(x = period, y = Inflacja, color = country,group = country)) +
@@ -92,3 +134,16 @@ ggplot(thm, aes(x = Year, y = coicop, fill = Inflacja)) +
   scale_fill_gradient(low = "white", high = "blue4") + 
   theme_minimal() 
   
+map_data_europe <- map_data("world")
+dane_mapa <- map_data_europe %>%
+  left_join(merged_map, by = c("region" = "Full"))
+
+ggplot(dane_mapa) +
+  geom_polygon(aes(x = long, y = lat, group = group, fill = Full_food), color = "black") +
+  scale_fill_viridis_d() +  
+  labs(title = "Food category whose price increased the most compared to the country") +
+  theme_minimal() +
+  theme(legend.position = "right") +
+  coord_cartesian(xlim = c(-30, 45), ylim = c(35, 70))
+
+
